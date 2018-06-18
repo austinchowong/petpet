3524e4971adf4fa72fde4d1b9145ac72fbfb73cc
Austin Cho-Wong <austin.cho.wong@gmail.com>
2018-06-17 21:15:01 -0400

    created UI elements and interactions for tomorrow's demo presentation

---
+++ app/src/main/java/petpet/petpet/Pet/WalkFragment.kt
@@ -0,0 +1,24 @@
+package petpet.petpet.Pet
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import petpet.petpet.R
+
+public class WalkFragment: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        var builder = AlertDialog.Builder(activity)
+        var inflater = activity.layoutInflater
+
+        builder.setTitle("Feed")
+                .setView(inflater.inflate(R.layout.fragment_walk, null))
+                .setPositiveButton("OK", { dialog, whichButton ->
+                    // do something
+                })
+
+        return builder.create()
+    }
+}
