9a9c71931cb09c305da5bcaf9f30f4a39095f2cc
Yunlin Lu <ilenelu@v1020-wn-201-34.campus-dynamic.uwaterloo.ca>
2018-06-17 22:28:27 -0400

    ui change

---
+++ app/src/main/java/petpet/petpet/Pet/petMenu.kt
@@ -0,0 +1,16 @@
+package petpet.petpet.Pet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import petpet.petpet.R
+import kotlinx.android.synthetic.main.activity_pet_menu.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class petMenu : AppCompatActivity() {
+
+}
